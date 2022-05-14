940064c36d029e5d863f0b93bc4f10dd9bad6c84
Frukha <Frukha@hotmail.com>
2022-05-14 11:45:10 +0200

    Project_Chuck

--- app/src/main/java/com/example/projectchucknorris/MainActivity.kt
+++ app/src/main/java/com/example/projectchucknorris/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Single
 import kotlinx.serialization.*
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.*
 
 
@@ -20,19 +23,38 @@ class MainActivity : AppCompatActivity() {
         Log.d("jokes", Jokes.list_jokes.toString())
         val recyclerView = findViewById<RecyclerView>(R.id.list_)
         recyclerView.adapter = JokeAdapter()
+        retrieveJoke()
     }
-}
 
-object Jokes {
-    val list_jokes = listOf<String> ("Chuck Norris can run so fast, he can actually shoot an apple off of his own head",
-        "Chuck Norris doesn't feel your pain... he causes it",
-        "Chuck Norris uses pepper spray to spice up his steaks.",
-        "god said let there be light Chuck Norris said say please",
-        "when Chuck Norris is asleep the sheep count him",
-        "You can see Chuck Norris's charisma from space.",
-        "Chuck Norris' shadow follows him from a safe distance.",
-        "There is no theory of evolution, just a list of creatures Chuck Norris allows to live.",
-        "Chuck Norris never wet his bed as a child. The bed wet itself out of fear.",
-        "Chuck Norris doesn't wear sunscreen when he is in the sun. The Sun wears Chuck Norris Screen.")
-}
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
 
+
+    object Jokes {
+        val list_jokes = listOf<String>(
+            "Chuck Norris can run so fast, he can actually shoot an apple off of his own head",
+            "Chuck Norris doesn't feel your pain... he causes it",
+            "Chuck Norris uses pepper spray to spice up his steaks.",
+            "god said let there be light Chuck Norris said say please",
+            "when Chuck Norris is asleep the sheep count him",
+            "You can see Chuck Norris's charisma from space.",
+            "Chuck Norris' shadow follows him from a safe distance.",
+            "There is no theory of evolution, just a list of creatures Chuck Norris allows to live.",
+            "Chuck Norris never wet his bed as a child. The bed wet itself out of fear.",
+            "Chuck Norris doesn't wear sunscreen when he is in the sun. The Sun wears Chuck Norris Screen."
+        )
+    }
+
+    private fun retrieveJoke(): Disposable = jokeService.subscribeOn(Schedulers.io()).subscribeBy(
+        onError = { Log.d("error", "error detected") },
+        onSuccess = {
+            Log.d("success", "The joke is: ${it.value}")
+        }
+    ).also { compositeDisposable.add(it) }
+
+    val compositeDisposable = CompositeDisposable()
+
+    val jokeService: Single<Joke> = JokeApiServiceFactory.builderService().giveMeAJoke()
+}
