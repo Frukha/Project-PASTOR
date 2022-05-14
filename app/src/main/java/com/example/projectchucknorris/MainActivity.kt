b1e777415a42adb35bbcacb301cb661f21e55518
Frukha <Frukha@hotmail.com>
2022-05-14 18:18:01 +0200

    Project_Chuck

--- app/src/main/java/com/example/projectchucknorris/MainActivity.kt
+++ app/src/main/java/com/example/projectchucknorris/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.projectchucknorris
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Single
@@ -14,7 +16,7 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.*
-
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -49,17 +51,28 @@ class MainActivity : AppCompatActivity() {
         )
     }*/
 
-    private fun retrieveJoke(): Disposable = jokeService.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribeBy(
-        onError = { Log.d("error", "error detected") },
-        onSuccess = {
-            Log.d("success", "The joke is: ${it.value}")
-            adapter.addJoke(it)
-        }
-    ).also { compositeDisposable.add(it) }
+    private fun retrieveJoke(){
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar_id)
+        progressBar.visibility = View.VISIBLE
+        jokeService.subscribeOn(Schedulers.io()).repeat(10).delay(1000, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).subscribeBy(
+            onError = { Log.d("error", "error detected")
+                progressBar.visibility = View.INVISIBLE},
+            onNext = {
+                Log.d("success", "The joke is: ${it.value}")
+                adapter.addJoke(it)
+                progressBar.visibility = View.INVISIBLE
+            },
+            onComplete = {
+                Log.d("finished", "10 jokes were shown")
+            }
+        ).also { compositeDisposable.add(it) }
+    }
 
     val compositeDisposable = CompositeDisposable()
 
     val jokeService: Single<Joke> = JokeApiServiceFactory.builderService().giveMeAJoke()
 
     val adapter = JokeAdapter()
+
+
 }
