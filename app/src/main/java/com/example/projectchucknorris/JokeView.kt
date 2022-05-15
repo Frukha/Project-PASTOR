87b863c3633b1a488a42fdda50d05a5ceca96275
Frukha <Frukha@hotmail.com>
2022-05-15 19:03:11 +0200

    Project_Chuck

--- app/src/main/java/com/example/projectchucknorris/JokeView.kt
+++ app/src/main/java/com/example/projectchucknorris/JokeView.kt
@@ -1,4 +1,12 @@
 package com.example.projectchucknorris
 
-class JokeView: {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class JokeView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): ConstraintLayout(context, attrs, defStyleAttr) {
+
+    //val view: ConstraintLayout = LayoutInflater.from(parent.context).inflate(R.layout.joke_layout, parent, false) as ConstraintLayout
+
 }
